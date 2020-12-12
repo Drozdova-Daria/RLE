d8d5b7374f8b55cb2db64dbc7c3e23412888bb2f
Drozdova-Daria <60818854+Drozdova-Daria@users.noreply.github.com>
2020-12-12 15:43:49 +0300

    Update ConfigRider.java

--- ConfigRider.java
+++ ConfigRider.java
@@ -11,14 +11,14 @@ enum ConfigGrammar {
     OUTPUT_FILE,
     LENGTH_SEQUENCE,
     BUFFER_SIZE,
-    NUMBER_SYSTEM,
     ACTION;
     public static final String delimiter = " = ";
 }
 
 public class ConfigRider {
     private String input, output, action;
-    private int lengthSequence, bufferSize, numberSystem;
+    private int lengthSequence, bufferSize;
+
     ConfigRider(String fileName) {
         try {
             FileReader reader = new FileReader(fileName);
@@ -27,19 +27,19 @@ public class ConfigRider {
                 String[] parameter = config.nextLine().split(ConfigGrammar.delimiter);
                 ReadParameters(parameter);
             }
-            if(bufferSize < lengthSequence) {
+            if (bufferSize < lengthSequence) {
                 Errors.ERROR_CONFIG_LENGTH_BUFFER.PrintErrorInLog();
             }
         } catch (IOException exception) {
             Errors.ERROR_CONFIG_FILE_OPENING.PrintErrorInLog();
         }
     }
+
     private void ReadParameters(String[] parameter) {
-        if(parameter.length != 2) {
+        if (parameter.length != 2) {
             Errors.ERROR_CONFIG_PARAMETERS_LENGTH.PrintErrorInLog();
-        }
-        else {
-            switch(ConfigGrammar.valueOf(parameter[0])) {
+        } else {
+            switch (ConfigGrammar.valueOf(parameter[0])) {
                 case INPUT_FILE:
                     input = parameter[1];
                     break;
@@ -47,13 +47,10 @@ public class ConfigRider {
                     output = parameter[1];
                     break;
                 case LENGTH_SEQUENCE:
-                    lengthSequence = LengthInInt(parameter[1]);
+                    lengthSequence = parseInt(parameter[1]);
                     break;
                 case BUFFER_SIZE:
                     bufferSize = parseInt(parameter[1]);
-                    break;
-                case NUMBER_SYSTEM:
-                    numberSystem = parseInt(parameter[1]);
                     break;
                 case ACTION:
                     action = parameter[1];
@@ -65,29 +62,9 @@ public class ConfigRider {
         }
     }
 
-    String GetInputFile() {
-        return input;
-    }
-    String GetOutputFile() {
-        return output;
-    }
-    int GetLengthSequence() {
-        return lengthSequence;
-    }
+    String GetInputFile() { return input; }
+    String GetOutputFile() { return output; }
+    int GetLengthSequence() { return lengthSequence; }
     int GetBufferSize() { return bufferSize; }
     String GetAction() { return action; }
-
-    private int LengthInInt(String length) {
-        if(length.matches("-?[\\d]+")) {
-            int lengthInt = parseInt(length, 10);
-            if (lengthInt > 0 && lengthInt <= numberSystem) {
-                return lengthInt;
-            } else {
-                Errors.ERROR_LENGTH_LIMIT.PrintErrorInLog();
-            }
-        } else {
-            Errors.ERROR_LENGTH_TYPE.PrintErrorInLog();
-        }
-        return -1;
-    }
 }
